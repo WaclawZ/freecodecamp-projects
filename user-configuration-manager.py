4fa8070acd3182067747623369a8bf3f5a751389
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-04 19:21:46 +0200

    add update_setting function

--- user-configuration-manager.py
+++ user-configuration-manager.py
@@ -7,6 +7,16 @@ def add_setting(current_settings: dict, setting: tuple):
     else:
         current_settings.update({key: value})
         return f"Setting '{key}' added with value '{value}' successfully!"
+    
+def update_setting(current_settings: dict, setting: tuple):
+    key = str(setting[0]).lower()
+    value = str(setting[1]).lower()
+
+    if key.lower() in current_settings.keys():
+        current_settings.update({key: value})
+        return f"Setting '{key}' updated to '{value}' successfully!"
+    else:
+        return f"Setting '{key}' does not exist! Cannot update a non-existing setting."
 
 test_settings = {
     'theme': 'dark',
