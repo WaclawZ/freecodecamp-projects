de29326e939c35217cc1d16bc014ba71c9a0c1b9
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-04 19:07:28 +0200

    complete add_settings function

--- user-configuration-manager.py
+++ user-configuration-manager.py
@@ -1,5 +1,12 @@
 def add_settings(current_settings: dict, setting: tuple):
-    pass
+    key = str(setting[0]).lower()
+    value = str(setting[1]).lower()
+    
+    if key.lower() in current_settings.keys():
+        return f"Setting '{key}' already exists! Cannot add a new setting with this name."
+    else:
+        current_settings.update({key: value})
+        return f"Setting '{key}' added with value '{value}' successfully!"
 
 test_settings = {
     'theme': 'dark',
