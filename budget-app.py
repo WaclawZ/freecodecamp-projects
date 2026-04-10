f2c69c8e7e9e68e396750da0c7097ea95fb76696
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-10 22:44:15 +0200

    create deposit method

--- budget-app.py
+++ budget-app.py
@@ -3,5 +3,9 @@ class Category:
         self.name = name
         self.ledger = []
 
+    def deposit(self, amount, descrption=""):
+        self.ledger.append({"amount": amount, "description": descrption})
+
+
 def create_spend_chart(categories):
     pass
