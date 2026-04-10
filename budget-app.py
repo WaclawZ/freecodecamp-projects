eaaf1604478d85c4042e15ea96debb9ddaa3273c
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-10 22:55:58 +0200

    create withdraw method

--- budget-app.py
+++ budget-app.py
@@ -3,17 +3,23 @@ class Category:
         self.name = name
         self.ledger = []
 
-    def deposit(self, amount, descrption=""):
-        self.ledger.append({"amount": amount, "description": descrption})
+    def deposit(self, amount, description=""):
+        self.ledger.append({"amount": amount, "description": description})
 
     def get_balance(self):
         balance = 0
 
         for entry in self.ledger:
-            print(entry)
             balance += entry['amount']
 
         return balance
+    
+    def withdraw(self, amount, description=""):
+        if amount <= self.get_balance():
+            self.ledger.append({"amount": -amount, "description": description})
+            return True
+        else:
+            return False
 
 
 def create_spend_chart(categories):
