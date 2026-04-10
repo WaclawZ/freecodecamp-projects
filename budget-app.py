11f31bcc48e9a0ed622697642ef45ed86ce94070
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-10 23:30:01 +0200

    create __str__ method

--- budget-app.py
+++ budget-app.py
@@ -22,18 +22,37 @@ class Category:
             return False
 
     def transfer(self, amount, category):
-        if(self.check_funds(amount)):
-            self.withdraw(amount, f"Transfer to: {category.name}")
-            category.deposit(amount, f"Transfer from: {self.name}")
+        if self.check_funds(amount):
+            self.withdraw(amount, f"Transfer to {category.name}")
+            category.deposit(amount, f"Transfer from {self.name}")
             return True
         else:
             return False
-    
+
     def check_funds(self, amount):
         if self.get_balance() < amount:
             return False
         else:
             return True
+
+    def __str__(self):
+        name_length = len(self.name)
+        result = (
+            "*" * (15 - name_length // 2)
+            + self.name
+            + "*" * (15 - name_length // 2)
+            + "\n"
+        )
+
+        for entry in self.ledger:
+            des = entry["description"][:23]
+            amo = str(entry["amount"])
+            result += des + " " * (30 - len(des) - len(amo)) + amo + "\n"
+
+        result += f"Total: {self.get_balance()}"
+
+        return result
+
 
 def create_spend_chart(categories):
     pass
