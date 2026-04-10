e26f526e39c5434388013a562bceb40af71d3efc
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-10 22:51:07 +0200

    create get_balance method

--- budget-app.py
+++ budget-app.py
@@ -6,6 +6,15 @@ class Category:
     def deposit(self, amount, descrption=""):
         self.ledger.append({"amount": amount, "description": descrption})
 
+    def get_balance(self):
+        balance = 0
+
+        for entry in self.ledger:
+            print(entry)
+            balance += entry['amount']
+
+        return balance
+
 
 def create_spend_chart(categories):
     pass
