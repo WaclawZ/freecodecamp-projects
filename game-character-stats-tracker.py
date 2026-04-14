e56700d68ea2e1ab5e488842905ff86cf94ec2fb
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-14 21:30:30 +0200

    format code with Ruff

--- game-character-stats-tracker.py
+++ game-character-stats-tracker.py
@@ -8,21 +8,23 @@ class GameCharacter:
     @property
     def name(self):
         return self._name
-    
+
     @property
     def health(self):
         return self._health
-    
+
     @health.setter
     def health(self, new_health):
         if new_health > 100 or new_health < 0:
-            raise ValueError("Health must be a value greater than 0 and lesser than 100")
+            raise ValueError(
+                "Health must be a value greater than 0 and lesser than 100"
+            )
         self._health = new_health
 
     @property
     def mana(self):
         return self._mana
-    
+
     @mana.setter
     def mana(self, new_mana):
         if new_mana > 50 or new_mana < 0:
