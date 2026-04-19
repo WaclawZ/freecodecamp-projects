76a88be80966cd806f94b87a7970921708f1b43a
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-19 16:53:23 +0200

    implement str method

--- polygon-area-calculator.py
+++ polygon-area-calculator.py
@@ -2,3 +2,6 @@ class Rectangle:
     def __init__(self, width: int, height: int) -> None:
         self.width = width
         self.height = height
+
+    def __str__(self) -> str:
+        return f"Rectangle(width={self.width}, height={self.height})"
