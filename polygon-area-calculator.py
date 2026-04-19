ad21787f5aba8101e7c6f4e05023da15cb4b2ce0
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-19 16:56:01 +0200

    create required methods of Rectangle class

--- polygon-area-calculator.py
+++ polygon-area-calculator.py
@@ -5,3 +5,24 @@ class Rectangle:
 
     def __str__(self) -> str:
         return f"Rectangle(width={self.width}, height={self.height})"
+
+    def set_width():
+        pass
+
+    def set_height():
+        pass
+
+    def get_area():
+        pass
+
+    def get_perimeter():
+        pass
+
+    def get_diagonal():
+        pass
+
+    def get_picture():
+        pass
+
+    def get_amount_inside():
+        pass
