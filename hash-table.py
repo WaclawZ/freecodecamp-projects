deec92f83a9d82c30a397137033164728ae5f2a2
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-24 20:20:04 +0200

    refactor to fix error

--- hash-table.py
+++ hash-table.py
@@ -11,18 +11,18 @@ class HashTable:
         if hash_key in self.collection:
             self.collection[hash_key][key] = value
         else:
-            self.collection[hash_key] = value
+            self.collection[hash_key] = {key: value}
 
     def remove(self, key: str) -> None:
         hash_key = self.hash(key)
 
-        if hash_key in self.collection:
-            del self.collection[hash_key]
+        if hash_key in self.collection and key in self.collection[hash_key]:
+            self.collection[hash_key].pop(key)
 
     def lookup(self, key: str) -> None:
         hash_key = self.hash(key)
 
-        if hash_key in self.collection:
-            return self.collection[hash_key]
+        if hash_key in self.collection and key in self.collection[hash_key]:
+            return self.collection[hash_key][key]
         else:
             return None
