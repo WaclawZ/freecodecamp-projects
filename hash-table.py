5838d4f8a661c6949d3895fa52430b9b50d76bd0
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-24 20:00:55 +0200

    implement add method

--- hash-table.py
+++ hash-table.py
@@ -2,11 +2,16 @@ class HashTable:
     def __init__(self) -> None:
         self.collection = {}
 
-    def hash(value: str) -> int:
+    def hash(self, value: str) -> int:
         return sum([ord(c) for c in value])
 
-    def add():
-        pass
+    def add(self, key: str, value) -> None:
+        hash_value = hash(key)
+
+        if hash_value in self.collection:
+            self.collection[hash_value][key] = value
+        else:
+            self.collection[hash_value] = value
 
     def remove():
         pass
