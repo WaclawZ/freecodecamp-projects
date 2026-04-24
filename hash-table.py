0ecd427f2ef43c3aed2ac21a54ddcde6e58eb536
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-24 22:22:05 +0200

    refactor

--- hash-table.py
+++ hash-table.py
@@ -3,26 +3,17 @@ class HashTable:
         self.collection = {}
 
     def hash(self, value: str) -> int:
-        return sum([ord(c) for c in value])
+        return sum(ord(c) for c in value)
 
     def add(self, key: str, value) -> None:
         hash_key = self.hash(key)
-
-        if hash_key in self.collection:
-            self.collection[hash_key][key] = value
-        else:
-            self.collection[hash_key] = {key: value}
+        self.collection.setdefault(hash_key, {})[key] = value
 
     def remove(self, key: str) -> None:
         hash_key = self.hash(key)
+        if hash_key in self.collection:
+            self.collection[hash_key].pop(key, None)
 
-        if hash_key in self.collection and key in self.collection[hash_key]:
-            self.collection[hash_key].pop(key)
-
-    def lookup(self, key: str) -> None:
+    def lookup(self, key: str):
         hash_key = self.hash(key)
-
-        if hash_key in self.collection and key in self.collection[hash_key]:
-            return self.collection[hash_key][key]
-        else:
-            return None
+        return self.collection.get(hash_key, {}).get(key)
