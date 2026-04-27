75719943397cfaf191590375de49f773c796de3e
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-27 11:15:52 +0200

    implement selection_sort method

--- selection-sort-algorithm.py
+++ selection-sort-algorithm.py
@@ -1,2 +1,9 @@
 def selection_sort(numbers: list) -> list:
-    pass
+    for i in range(len(numbers)):
+        min_index = i
+        for j in range(i + 1, len(numbers)):
+            if numbers[min_index] > numbers[j]:
+                min_index = j
+        if min_index != i:
+            numbers[min_index], numbers[i] = numbers[i], numbers[min_index]
+    return numbers
