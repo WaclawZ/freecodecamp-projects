9c2cedf3328c8a443f169ab10113ee3b7170dd2b
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-27 14:54:01 +0200

    implement hanoi_solver

--- tower-of-hanoi.py
+++ tower-of-hanoi.py
@@ -1,2 +1,21 @@
 def hanoi_solver(num: int) -> str:
-    pass
+    first = [n for n in range(num, 0, -1)]
+    second, third = [], []
+
+    steps = []
+
+    def record_state():
+        steps.append(f"{first} {second} {third}")
+
+    record_state()
+
+    def move(n, source, target, temp):
+        if n > 0:
+            move(n - 1, source, temp, target)
+            target.append(source.pop())
+            record_state()
+            move(n - 1, temp, target, source)
+
+    move(num, first, third, second)
+
+    return "\n".join(steps)
