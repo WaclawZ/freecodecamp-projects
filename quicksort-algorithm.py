d5ed1b931041047beaa96a221e91d132c402db35
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-27 10:37:11 +0200

    implement quick_sort method

--- quicksort-algorithm.py
+++ quicksort-algorithm.py
@@ -1,2 +1,17 @@
 def quick_sort(numbers: list) -> list:
-    pass
+    
+    if len(numbers) < 2:
+        return numbers
+
+    pivot = numbers[0]    
+    less, equal, greater = [], [], []
+
+    for num in numbers:
+        if num < pivot:
+            less.append(num)
+        elif num > pivot:
+            greater.append(num)
+        else:
+            equal.append(num)
+    
+    return quick_sort(less) + equal + quick_sort(greater)
