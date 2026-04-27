58fc9aa8c554fe03f4b76bfc84798e4eaee4ad54
WaclawZ <waclaw.zalicki@gmail.com>
2026-04-27 13:31:29 +0200

    implement verify_card_number method

--- luhn-algorithm.py
+++ luhn-algorithm.py
@@ -1,2 +1,13 @@
 def verify_card_number(card_number):
-    pass
+    filtered_string = list(filter(lambda c: c.isdigit(), card_number))
+    reversed_string = filtered_string[::-1]
+    checksum = 0
+    for i, num in enumerate(reversed_string):
+        num = int(num)
+        if i % 2 == 1:
+            num *= 2
+            if num > 9:
+                num -= 9
+        checksum += num
+
+    return "VALID!" if checksum % 10 == 0 else "INVALID!"
