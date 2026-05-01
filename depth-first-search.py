584d5f74edabd2442a10e7b6635acae6fe7b29cb
WaclawZ <waclaw.zalicki@gmail.com>
2026-05-01 18:41:10 +0200

    implement dfs

--- depth-first-search.py
+++ depth-first-search.py
@@ -1,2 +1,13 @@
 def dfs(matrix: list, node_no: int) -> list:
-    pass
+    stack = [node_no]
+    visited = []
+
+    while stack:
+        current = stack.pop()
+        visited.append(current)
+
+        for node, edge in enumerate(matrix[current]):
+            if edge == 1 and node not in visited:
+                stack.append(node)
+
+    return visited
