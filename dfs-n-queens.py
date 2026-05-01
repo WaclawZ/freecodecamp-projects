a18d0f88a31aa650cc28ed574d826ea85a6e4e58
WaclawZ <waclaw.zalicki@gmail.com>
2026-05-01 19:02:45 +0200

    implement dfs-n-queens function

--- dfs-n-queens.py
+++ dfs-n-queens.py
@@ -1,2 +1,34 @@
 def dfs_n_queens(n: int) -> list:
-    pass
+    if n < 1:
+        return []
+
+    solutions = []
+
+    def is_safe(current_board, current_row, col):
+        for r in range(current_row):
+            c = current_board[r]
+
+            if c == col:
+                return False
+
+            if abs(c - col) == abs(r - current_row):
+                return False
+
+        return True
+
+    def dfs(row, current_board):
+        if row == n:
+            solutions.append(current_board[:])
+            return
+
+        for col in range(n):
+            if is_safe(current_board, row, col):
+                current_board.append(col)
+
+                dfs(row + 1, current_board)
+
+                current_board.pop()
+
+    dfs(0, [])
+
+    return solutions
