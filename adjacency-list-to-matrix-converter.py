281ae408fe325b0b7d365175aabafb632630e74a
WaclawZ <waclaw.zalicki@gmail.com>
2026-05-01 15:18:15 +0200

    implement adjacency_list_to_matrix method

--- adjacency-list-to-matrix-converter.py
+++ adjacency-list-to-matrix-converter.py
@@ -1,2 +1,9 @@
-def adjacency_list_to_matrix():
-    pass
+def adjacency_list_to_matrix(dictionary: dict) -> list:
+    dict_len = len(dictionary)
+    matrix = [[0] * dict_len for _ in range(dict_len)]
+
+    for key, value in dictionary.items():
+        for el in value:
+            matrix[key][el] = 1
+
+    return matrix
